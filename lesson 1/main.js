6e29e57d463c9cda0acb58ae3997546bb1e8e36b
fat's not fat <phatnguyenzn12@gmail.com>
2020-12-07 08:44:57 +0700

    hello kitty

---
+++ lesson 1/main.js
@@ -0,0 +1,20 @@
+//hàm alert là hàm hiện cửa sổ thông báo
+//syntax: cú pháp
+//varilable: biến
+
+/**
+ * alert là hàm hiện cửa sổ thông báo
+ * console chứa nhiều hàm bên trong, gọi là phương thức
+ * confirm
+ * prompt kết hợp alert vs confirm
+ * set timeout chạy 1 đoạn code trong 1 khoảng thời gian và chỉ chạy 1 lần
+ * set interval chạy 1 đoạn code trong 1 khoảng thời gian và liên tục lặp đi lặp lại
+ */
+
+
+/**
+ * toán tử số học - Arithmetic
+ * toán tử gán - Assignment
+ * toasn tử so sánh - Comparrison
+ * toán tử logic - Logical
+ */
